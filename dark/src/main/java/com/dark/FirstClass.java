19dd4476155fe9fa21107913ca397f4fa9a3be35
ark0000 <karthikarun407@gmail.com>
2024-03-09 16:12:08 +0530

    This project is basic SPRING BOOT with basic understandings This is first way of bean

---
+++ dark/src/main/java/com/dark/FirstClass.java
@@ -0,0 +1,10 @@
+package com.dark;
+
+import org.springframework.stereotype.Component;
+
+
+public class FirstClass {
+    public  String sayhello(){
+        return "Hello";
+    }
+}
--- dark/src/main/java/com/dark/Main.java
+++ dark/src/main/java/com/dark/Main.java
@@ -3,50 +3,29 @@ package com.dark;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class,args);
-    }
-    @GetMapping("/")
-    public String greet(@RequestParam(value = "name",defaultValue = "Hello") String nameData){
-        return "Hello";
-    }
 
+        //SpringApplication.run(Main.class,args);---1
+        //This return ConfigurableAppcontext
+        var ctx=SpringApplication.run(Main.class,args);
+//        FirstClass  f=new FirstClass();---1
+        FirstClass myFirstClassObject=ctx.getBean(FirstClass.class);
+        System.out.println(myFirstClassObject.sayhello());
+    }
+@Bean
+public FirstClass myFirstClassObject(){
+        return new FirstClass();
+}
 }
 
-
-/*
-* @SpringBootApplication
-*SPringBootApplication
-* @configuration@Enableautocofig@componentscan
-*config the class as appcoonfig
-* create beans
-* bean dependant on another bean
-* db username psswd class binds into bean
-*@Enableautocofig
-*makes spring to guess the config based on jar filrs
-* figureout whatlib abd pre config
-* @ComponentScan
-* tells spring search for component
-*@Configuration bean to instatiate
-*
-* @ComponentScan(basePackages = "com.dark")
-*
-* */
-/*
-@Confifguration as appconfig
-* db of connections usr pssd
-* class that binds properties into your class or bean
-@EnableAutoConfig
-* makes spring to guess the config based on jar files on CP
-@ComponentScan
-*look for components
-default where manin class is search
-*/
